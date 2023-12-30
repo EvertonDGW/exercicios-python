235a34d9b285b6fc63bedd080b94a0b11b27046d
EvertonRobertoBR <154098335+EvertonRobertoBR@users.noreply.github.com>
2023-12-30 20:31:02 -0300

    ex11

---
+++ ex11.py
@@ -0,0 +1,4 @@
+n1 = float(input('Digite a primeira nota do aluno:'))
+n2 = float(input('Digite a segunda nota do aluno:'))
+m = ((n1 + n2) / 2) #colocamos parenteses para que a soma seja feita primeiro
+print('A média entre {} e {} é igual a {}' .format(n1, n2, m))
