f3a4f8f61835be922b519429c9263ed3324c85ce
EvertonRobertoBR <154098335+EvertonRobertoBR@users.noreply.github.com>
2023-12-30 20:29:23 -0300

    ex8

---
+++ ex8.py
@@ -0,0 +1,4 @@
+n=int(input('digite um numero:'))
+print('analisando o valor {}, seu antecessor é {}, seu sucessor é {}'.format(n, n-1, n+1))
+
+#codigo semelhante ao ex7, mas com uma diferença no print
