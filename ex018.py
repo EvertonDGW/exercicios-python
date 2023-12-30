3ef825fad512b52d848482bfe9337f022ddcede9
EvertonRobertoBR <154098335+EvertonRobertoBR@users.noreply.github.com>
2023-12-30 20:57:49 -0300

    ex18

---
+++ ex018.py
@@ -0,0 +1,5 @@
+c=float(input('informe a temperatura em °C:  ')) # °C = celsius 
+f= (( 9 * c) /5) + 32   # se voce quiser pode tirar os parenteses que estão no (9 * 5)
+print('a temperatura de {} °C corresponde a {} F'.format(c,f)) #F = fahrenheit
+
+#convertemos de celsius para fahrenheit
