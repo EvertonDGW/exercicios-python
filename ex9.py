0cc0c852034d64f0b3278346971c792093d42315
EvertonRobertoBR <154098335+EvertonRobertoBR@users.noreply.github.com>
2023-12-30 20:29:44 -0300

    ex9

---
+++ ex9.py
@@ -0,0 +1,6 @@
+n = int(input('digite um numero:'))
+d = n * 2
+t = n * 3
+r = n ** (1/2)  # primeiro vai ser feito a divisão (1/2)
+print('o dobro de {} é {},'.format(n, d))
+print('o triplo de {} é {},\n a raiz quadrada de {} é {:.2f}'.format(n, t, n, r))
