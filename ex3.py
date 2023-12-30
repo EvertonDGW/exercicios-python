4f791f459bfe4287875f7cc7d3ec89a4b6e2601d
EvertonRobertoBR <154098335+EvertonRobertoBR@users.noreply.github.com>
2023-12-30 20:27:06 -0300

    ex3

---
+++ ex3.py
@@ -0,0 +1,6 @@
+n1 = int(input('digite um numero:'))                
+n2 = int(input('digite outro numero:'))             
+s = n1+n2
+print('a soma vale {}'.format(s))
+
+#exercicio semelhante ao ex2, mas com uma diferença na linha(4) do codigo print
