df5fdb60816f49773a2f6f11df65e2fd31bfe8bb
EvertonRobertoBR <154098335+EvertonRobertoBR@users.noreply.github.com>
2023-12-30 20:26:12 -0300

    ex2

---
+++ ex2.py
@@ -0,0 +1,7 @@
+n1 = int(input('digite um numero:'))                #int significa numero inteiro exemplo -> 7, -4, 0, 9823
+n2 = int(input('digite outro numero:'))             #float sigifica numeros reais ou ponto flutuante exemplo -> 4.5, 0.045, -12.345, 3.0
+s = n1+n2                                           #bool significa valores logicos ou booleanos exemplo -> True ou False, a primeira letra sempre é maiuscula
+print('a soma vale', s)                             #str significa strings exemplo -> 'ola'
+
+
+
