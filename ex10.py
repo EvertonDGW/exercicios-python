dcc8dde022d12bacb690b56b7c9a051364597fe3
EvertonRobertoBR <154098335+EvertonRobertoBR@users.noreply.github.com>
2023-12-30 20:30:43 -0300

    ex10

---
+++ ex10.py
@@ -0,0 +1,7 @@
+n = int(input('digite um numero:'))
+print('o dobro de {} é {},'.format(n, n*2))
+print('o triplo de {} é {},\n a raiz quadrada de {} é {:.2f}'.format(n, n*3, n, n**(1/2))) #primeiro vai ser feito a divisão 1/2
+
+# o codigo n**(1/2) que foi usado para calcular a raiz quadrada, pode ser trocado por -> pow(n, 1/2). essa função pow vai calcular a raiz quadrada                                   
+
+#e o mesmo codigo do ex9 a diferença esta na linha 2 e 3 deste codigo
