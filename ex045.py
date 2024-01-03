00471ed28ea0225b9c0cdcfceac3ea1beb3b84c5
EvertonRobertoBR <154098335+EvertonRobertoBR@users.noreply.github.com>
2024-01-02 21:18:25 -0300

    atualizado

---
+++ ex045.py
@@ -0,0 +1,8 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor >= segundo_valor:
+    print('{} e maior ou igual a {}'.format(primeiro_valor,segundo_valor))
+    
+else:
+      print('{} e maior que {}'.format(segundo_valor,primeiro_valor))
---
+++ ex40.py
@@ -0,0 +1,8 @@
+a = 'AAAAA'
+b = 'BBBBBB'
+c = 1.1
+revelado=('b = {} a = {} a = {} c = {:.2f}'.format(b,a,a,c))
+print(revelado)
+
+   
+
